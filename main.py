92f524e7f0c7a40f784b97c8b2e2098cb1c605b5
Sergei Poluektov <seregatipich@outlook.com>
2023-07-03 16:18:23 +0200

    changed bot mesages

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.types import ReplyKeyboardRemove
 from dotenv import load_dotenv
 
 from google_handler import save_data
@@ -30,12 +29,12 @@ class Form(StatesGroup):
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message) -> None:
-    await message.reply('Привет!\nОтветь на несколько вопросов, что бы попасть в группу. Чтобы начать опрос, нажми на 👉🏻 /nachat_opros.')
+    await bot.send_message(message.chat.id, 'Привет!\nОтветь на несколько вопросов, чтобы попасть в группу.\nНажми на 👉🏻 /nachat_opros.')
 
 
 @dp.message_handler(commands=['nachat_opros'])
 async def start_poll(message: types.Message) -> None:
-    await message.reply('Как вас зовут?')
+    await bot.send_message(message.chat.id, 'Как вас зовут?')
     await Form.fullname.set()
 
 
@@ -43,7 +42,7 @@ async def start_poll(message: types.Message) -> None:
 async def process_fullname(message: types.Message, state: FSMContext) -> None:
     async with state.proxy() as data:
         data['fullname'] = message.text
-    await message.reply("Ваш номер телефона:")
+    await bot.send_message(message.chat.id, "Ваш номер телефона:")
     await Form.next()
 
 
@@ -51,7 +50,7 @@ async def process_fullname(message: types.Message, state: FSMContext) -> None:
 async def process_phone(message: types.Message, state: FSMContext) -> None:
     async with state.proxy() as data:
         data['phone'] = message.text
-    await message.reply("Ваш Email:")
+    await bot.send_message(message.chat.id, "Ваш E-mail:")
     await Form.next()
 
 
@@ -59,7 +58,7 @@ async def process_phone(message: types.Message, state: FSMContext) -> None:
 async def process_email(message: types.Message, state: FSMContext) -> None:
     async with state.proxy() as data:
         data['email'] = message.text
-    await message.reply("Ссылка на ваши социальные сети:")
+    await bot.send_message(message.chat.id, "Ссылка на ваши социальные сети:")
     await Form.next()
 
 
@@ -67,7 +66,7 @@ async def process_email(message: types.Message, state: FSMContext) -> None:
 async def process_social_media(message: types.Message, state: FSMContext) -> None:
     async with state.proxy() as data:
         data['social_media'] = message.text
-    await message.reply("Какая у вас профессия?")
+    await bot.send_message(message.chat.id, "Какая у вас профессия?")
     await Form.next()
 
 
@@ -75,17 +74,17 @@ async def process_social_media(message: types.Message, state: FSMContext) -> Non
 async def process_profession(message: types.Message, state: FSMContext) -> None:
     async with state.proxy() as data:
         data['profession'] = message.text
-    await message.reply("Чем бы вы хотели помогать людям?")
+    await bot.send_message(message.chat.id, "Чем бы вы хотели помогать людям?")
     await Form.next()
 
 
 @dp.message_handler(state=Form.help)
 async def process_help(message: types.Message, state: FSMContext) -> None:
+    await bot.send_message(message.chat.id, f"Спасибо за участие в опросе! Чтобы вступить в группу перейдите по ссылке: {PRIVATE_GROUP_LINK}")
     async with state.proxy() as data:
         data['help'] = message.text
     save_data(dict(data))
     await state.finish()
-    await bot.send_message(message.chat.id, f"Спасибо за участие в опросе! Вы можете присоединиться к нашей группе по ссылке: {PRIVATE_GROUP_LINK}")
 
 
 if __name__ == '__main__':
